00868f9b6a03a16d5a4274e476a694ac310bed21
cjans726 <cjans726@bjalearn.net>
2026-02-18 16:33:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ def calculate_total_price(products):
 def apply_discount(price, discount):
     if discount > 1:
         discount = discount / 100
-    return price - price * discount
+    return price - (price * discount)
 
 
 def update_stock(products, product_name, amount):
-    if isinstance(amount, str):
-        amount = int(amount) if amount.isdigit() else 0
-
     for product in products:
         if product["name"] == product_name:
-            product["stock"] = product["stock"] - amount
+            if product["stock"] >= amount:
+                product["stock"] -= amount
+    return product
 
 
 def get_average_price(products):
@@ -27,59 +26,51 @@ def get_average_price(products):
 
 def print_products(products):
     for i in range(len(products)):
-        print(products[i]["name"], "-", products[i]["price"], "-", products[i].get("stock", "N/A"))
+        print(
+            products[i]["name"], 
+            "-", 
+            products[i]["price"], 
+            "-", 
+            products[i]["stock"]
+        )
 
 
 products = [
     {"name": "Pencil", "price": 0.99, "stock": 100},
     {"name": "Notebook", "price": 2.50, "stock": 50},
     {"name": "Backpack", "price": 25, "stock": 20},
-    {"name": "Marker", "price": 1.5}
+    {"name": "Marker", "price": 1.5, "stock": 30}
 ]
 
 
 print("Product list:")
 print_products(products)
 
-
-print("Applying discount...")
-discounted = apply_discount(100, 20)
+print("\nApplying discount...")
+discounted = apply_discount(10, 20)
 print("Discounted price:", discounted)
 
-
-print("Updating stock...")
-update_stock(products, "Notebook", "5")
+print("\nUpdating stock...")
+update_stock(products, "Notebook", 5)
 print(products)
-
 
 average = get_average_price(products)
 print("Average price:", average)
 
-
-choice = input("Enter product name to buy: ")
-quantity_input = input("Enter quantity: ")
-
-if isinstance(quantity_input, str):
-    try:
-        quantity = int(quantity_input)
-    except ValueError:
-        quantity = 0
-else:
-    quantity = quantity_input
+choice = "Pencil"
+quantity = 2
 
 for product in products:
     if product["name"] == choice:
-        if quantity <= product["stock"]:
-            product["stock"] = product["stock"] - quantity
+        if int(quantity) <= product["stock"]:
+            product["stock"] = product["stock"] - int(quantity)
             print("Purchase successful")
         else:
             print("Not enough stock")
-
 
 count = 0
 while count < len(products):
     print(products[count]["name"])
     count = count + 1
-
 
 print("Done")
